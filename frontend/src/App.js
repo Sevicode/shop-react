b1e452de0fb29f6880dd8fae032827a1d4e07d1b
Sevda Amini <sevda.amini@hotmail.com>
2020-11-23 16:34:46 +1000

    adding bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -7,7 +8,10 @@ const App = () => {
     <>
       <header />
       <main>
+        <Container>
         <h1>Welcome to My Store!</h1>
+      </Container>
+
       </main>
       <footer />
     
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
 
 const Footer = () => {
     return (
         <footer>
-            footer
+            <Container>
+                <Row>
+                    <col className='text-center py-3 '>
+                        Copyright &copy; Sevda Amini-Uhde
+                    </col>
+                </Row>
+            </Container>           
         </footer>
     )
 }

e7a0c3324947c84489ec631fa81eda7a6967ff4e
Sevda Amini <sevda.amini@hotmail.com>
2020-11-23 21:14:57 +1000

    product information added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">My Store</Navbar.Brand>
+                    <Navbar.Brand href="/">PiGallery</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">

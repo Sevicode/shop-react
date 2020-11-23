e761b4d04c75c82d377997151983db01821d7070
Sevda Amini <sevda.amini@hotmail.com>
2020-11-23 16:00:15 +1000

    header and footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-    <h1>Welcome to My Store!</h1>
+      <header />
+      <main>
+        <h1>Welcome to My Store!</h1>
+      </main>
+      <footer />
+    
     </>
   );
 }
